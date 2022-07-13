8de7c6d5d95dff567ad378b6865a9ac07f569ae1
KapkaDibab <xk.8@yandex.ru>
2022-07-13 15:34:31 +0300

    hosts prereview

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,12 +13,17 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_string)
 
 DEBUG = False
 
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '158.160.4.31',
+]
 # ALLOWED_HOSTS = [
 #     '127.0.0.1',
 #     'localhost',
 #     '[::1]',
 # ]
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = [
 #     'esedess.sytes.net',
 #     '.esedess.sytes.net',

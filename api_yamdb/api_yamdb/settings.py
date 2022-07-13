2b9998eb0ba791925fd6e3c53a50652fa77d6797
KapkaDibab <xk.8@yandex.ru>
2022-07-13 15:27:03 +0300

    nginx + hosts

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,21 +13,21 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_string)
 
 DEBUG = False
 
-# ALLOWED_HOSTS = [
-#     '127.0.0.1',
-#     'localhost',
-#     '158.160.4.31',
-# ]
-# ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = [
-    'esedess.sytes.net',
-    '.esedess.sytes.net',
-    '10.129.0.18',
-    '158.160.4.31',
     '127.0.0.1',
     'localhost',
     '[::1]',
 ]
+# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = [
+#     'esedess.sytes.net',
+#     '.esedess.sytes.net',
+#     '10.129.0.18',
+#     '158.160.4.31',
+#     '127.0.0.1',
+#     'localhost',
+#     '[::1]',
+# ]
 # ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 # Application definition
